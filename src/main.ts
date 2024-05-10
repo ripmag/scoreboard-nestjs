bc417e25108b6faef06fdd55c10acb4e54abe365
Eduard <49712904+ripmag@users.noreply.github.com>
2024-05-10 23:44:40 +0500

    added PORT from env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => console.log('PORT is ====================================', port));
 }
 bootstrap();
