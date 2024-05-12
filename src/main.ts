aa6eb95d25052571ee0e494801b428708ce6d296
Eduard <49712904+ripmag@users.noreply.github.com>
2024-05-12 16:30:23 +0500

    change port to 8080

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   const port = process.env.PORT || 8080;
-  await app.listen(port, () => console.log('env.PORT is ====================================', port));
+  await app.listen(8080, () => console.log('env.PORT is ====================================', port));
 }
 bootstrap();
